dfa13d280027160dae569054412656366f0bc5d9
kye13 <kye135@naver.com>
2020-11-21 16:56:36 +0900

    로그인 컨피그 추가

--- src/main/java/com/example/demo/Membership2Application.java
+++ src/main/java/com/example/demo/Membership2Application.java
@@ -6,11 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.example.demo.db.MemberManager;
+import com.example.demo.vo.MemberVo;
+
 @SpringBootApplication
 public class Membership2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Membership2Application.class, args);
+		//MemberManager.insert(new MemberVo(5,"yd",PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("yd"),"영대","900912-1111111","010-9644-9912","kyd900@naver.com","우리집","user","o","o"));
 	}
 	
 	@Bean
